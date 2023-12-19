bf735570046ec33c2599af3bf748dfaaa3a08d7d
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-19 18:16:58 +0100

    add missing pip package and consider api in main.py

--- backend-services/prometheus-grafana-stack/python/hello-world-service/main.py
+++ backend-services/prometheus-grafana-stack/python/hello-world-service/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, status
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 @app.get('/api/v1/hws', status_code=200)
 async def hello():
