13c3fb33062953b10813ebf53fd3ed6f89340b54
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-20 00:00:30 +0100

    add rust actix-web considering prometheus scraper sample

---
+++ backend-services/prometheus-grafana-stack/rust/hello-world-service/src/main.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder, Result};
+use serde::Serialize;
+
+use actix_web_prom::{PrometheusMetrics, PrometheusMetricsBuilder};
+async fn health() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(Serialize)]
+struct HelloWorldResponse {
+    message: String,
+}
+
+#[get("/api/v1/hws")]
+async fn hello() -> Result<impl Responder> {
+    let obj = HelloWorldResponse {
+        message: String::from("Hello from Rust"),
+    };
+    Ok(web::Json(obj))
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let mut labels = HashMap::new();
+    labels.insert("label1".to_string(), "value1".to_string());
+    let prometheus = PrometheusMetricsBuilder::new("api")
+        .endpoint("/metrics")
+        .const_labels(labels)
+        .build()
+        .unwrap();
+    HttpServer::new(move || {
+        App::new().
+            wrap(prometheus.clone()).
+            service(web::resource("/health").to(health)).
+            service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
