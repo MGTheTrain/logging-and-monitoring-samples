21964c267f5bcf7dca449ca24f10a9b07e2cda22
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-20 17:13:08 +0100

    add simple python service considering loki

---
+++ backend-services/loki-grafana-stack/python/hello-world-service/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException, status
+import logging
+import logging_loki
+
+app = FastAPI()
+handler = logging_loki.LokiHandler(
+    url="https://my-loki-instance/loki/api/v1/push", 
+    tags={"application": "python-hello-world-service"},
+    # auth=("username", "password"),
+    version="1",
+)
+
+logger = logging.getLogger("hello-world-service-logger")
+logger.addHandler(handler)
+
+@app.get('/api/v1/hws', status_code=200)
+async def hello():
+    logger.info(
+        "Something happened", 
+        extra={"tags": {"service": "hello-world-service"}},
+    )
+    return { "message": "Hello, World from Python" } 
