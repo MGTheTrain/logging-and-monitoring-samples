06c472b4b662c0d2fe04dbed930506c4b4385b93
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-20 16:45:03 +0100

    add sample C# ASP .NET Cor sevice considering Loki dotnet package

---
+++ backend-services/loki-grafana-stack/c#/HelloWorldService/Web/Controllers/HelloWorlsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldService.Web.Controllers;
+
+[Route("api/v1/hws")]
+public class HelloWorldController : ControllerBase
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+    public HelloWorldController(){}   
+
+    /// <summary>
+    /// Hello world reponse endpoint 
+    /// </summary>
+    /// <response code="200">If operation succeeded</response>
+    /// <return>Returns awaitable task</return>
+    [HttpGet]
+    [Produces("application/json")]
+    public async Task<IActionResult> GetUploadBlobsMetainformation()
+    {
+        var jsonMessage = "{\"message\": \"Hello from C# ASP .NET Core\"}";
+        Logger.Info(jsonMessage);
+        return await Task.FromResult(StatusCode(200, jsonMessage));
+    }
+}
