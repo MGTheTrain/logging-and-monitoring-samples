f9389c9d36e8ad124bf60104333cb641f5b2c53a
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-20 19:28:24 +0100

    add crates and update main.rs considering loki

--- backend-services/loki-grafana-stack/rust/hello-world-service/src/main.rs
+++ backend-services/loki-grafana-stack/rust/hello-world-service/src/main.rs
@@ -2,6 +2,13 @@ use log::info;
 use actix_web::{get, web, App, HttpServer, HttpResponse, Responder, Result};
 use serde::Serialize;
 
+use log;
+use loki_logger;
+use log::LevelFilter;
+
+use std::env;
+use chrono::Utc;
+
 #[derive(Serialize)]
 struct HelloWorldResponse {
     message: String,
@@ -12,16 +19,24 @@ async fn hello() -> Result<impl Responder> {
     let obj = HelloWorldResponse {
         message: String::from("Hello from Rust"),
     };
-    info!("Hi");
+    // Get the current UTC time
+    let timestamp = Utc::now().to_rfc3339();
+    log::info!("{}: Hello from Rust", timestamp);
     Ok(web::Json(obj))
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    // Get the Loki URL from an environment variable or use a default value
+    let loki_url = match env::var("LOKI_URL") {
+        Ok(url) => url,
+        Err(_) => String::from("http://192.168.99.100:3100/loki/api/v1/push"), // Default URL
+    };
+
+    loki_logger::init(&loki_url, log::LevelFilter::Info).unwrap();
+
     HttpServer::new(move || {
-        App::new().
-            service(hello)
+        App::new().service(hello)
     })
     .bind(("0.0.0.0", 80))? // Listening on all available network interfaces or addresses on the machine
     .run()
