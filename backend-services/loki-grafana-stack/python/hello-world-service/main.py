be73d44d420271295f5a7d22947a1254474f8122
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-20 18:18:40 +0100

    update main.py

--- backend-services/loki-grafana-stack/python/hello-world-service/main.py
+++ backend-services/loki-grafana-stack/python/hello-world-service/main.py
@@ -1,22 +1,28 @@
 from fastapi import FastAPI, HTTPException, status
 import logging
 import logging_loki
+logging_loki.emitter.LokiEmitter.level_tag = "level"
 
 app = FastAPI()
-handler = logging_loki.LokiHandler(
-    url="https://my-loki-instance/loki/api/v1/push", 
+
+# Create a logger
+logger = logging.getLogger('python-hello-world-service-logger')
+logger.setLevel(logging.DEBUG)
+# Replace with your Loki server URL and port
+loki_url = 'http://192.168.99.100:3100/loki/api/v1/push'
+# Create a LokiHandler and set its URL
+loki_handler = logging_loki.LokiHandler(
+    url=loki_url,
     tags={"application": "python-hello-world-service"},
-    # auth=("username", "password"),
     version="1",
 )
-
-logger = logging.getLogger("hello-world-service-logger")
-logger.addHandler(handler)
+# Add the LokiHandler to the logger
+logger.addHandler(loki_handler)
 
 @app.get('/api/v1/hws', status_code=200)
 async def hello():
     logger.info(
-        { "message": "Hello, World from Python" }, 
+        "Hello, World from Python", 
         extra={"tags": {"service": "hello-world-service"}},
     )
     return { "message": "Hello, World from Python" } 
